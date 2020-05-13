da86b16a4a904fae47587fdd2b89da4a31b1aee3
Jose Franco <jose.franco@semperti.com>
2020-05-13 10:46:40 -0300

    changes

--- vars/jira.groovy
+++ vars/jira.groovy
@@ -36,7 +36,7 @@ def getIssueByVersion(project, version) {
     echo "Obteniendo tarjeta de version en JIRA: " + version
     SH_CMD = sh (
         script: "jiracli list --template table --query \"project = 'APDP' and affectedVersion IN ('8.0')  ORDER BY priority asc, created\" --endpoint=http://localhost:8088/ | grep -i APDP | awk -F '|' '{ print \$2 }' | sed -e 's/^[[:space:]]*//'",
-        returnStdout: false
+        returnStdout: true
     ).trim()
 
      
