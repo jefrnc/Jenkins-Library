494fc008e80561363a9f388cfb0d4d4aa9905be9
Jose Franco <jose.franco@semperti.com>
2020-05-13 11:41:44 -0300

    changes

---
+++ vars/utils.groovy
@@ -0,0 +1,38 @@
+#!/usr/bin/env groovy
+
+def getTag(tech) {
+    if (tech.equals("java"))
+        return readMavenPom().getVersion()
+    else if (tech.equals("nodejs")) 
+        return getVersionFromPackageJSON()
+}
+
+def getTagFromPom(folder){
+    def matcher = readFile(folder + 'pom.xml') =~ '<version>(.+)</version>'
+    matcher ? matcher[0][1] : null
+}
+
+def getVersionFromPackageJSON() {
+    sh "cat package.json | grep version | head -1 | awk -F: '{ print \$2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]' > version"
+
+    return readFile("version").trim()
+}
+
+def version(folder) {
+    dir("application"){
+        def matcher = readFile(folder + 'pom.xml') =~ '<version>(\\d*)\\.(\\d*)\\.(\\d*)(-SNAPSHOT)*</version>'
+        matcher ? matcher[0] : null
+    }
+}
+
+def IfConfirm(field, property, value) {
+    if (field && !field.empty) {
+        if (field[property]) {
+            if (field[property]==value ) {
+                return true
+            }
+        }
+         
+    }
+     return false
+}
