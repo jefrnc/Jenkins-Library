5e0483fb702b2a44383e9a50e558362bafea0cb7
Jose Franco <jose.franco@semperti.com>
2020-05-19 17:16:43 -0300

    test

--- vars/jira.groovy
+++ vars/jira.groovy
@@ -8,6 +8,66 @@ def getTag(tech) {
 }
 
 def login(username){
-   sh "jiracli login -u "+ username +"  --endpoint=" + env.JIRA_SERVER
+
+    SH_CMD = sh (
+        script: "jiracli login -u "+ username +"  --endpoint=" + env.JIRA_SERVER,
+        returnStdout: true
+    ).trim()
+
+    echo "Verificando conexion con JIRA: ${SH_CMD}"
 }
+
+def saveCredentials(pwd){
+   sh "echo \""+ pwd +"\" | pass insert jira -e"
+}
+
+
+def requestApproval(project, issue, envName,  user) {
+  sh "jiracli request -M POST rest/api/2/issue/"+ issue + "/comment '{\"body\":\"El usuario " + user + " solicito al aprobacion al ambiente "+ envName + ".\" }' --endpoint=" + env.JIRA_SERVER
+}
+
+def saveApproval(project, issue, envName,  user) {
+    sh "jiracli request -M POST rest/api/2/issue/"+ issue + "/comment '{\"body\":\"El usuario " + user + " acepto el pasaje a "+ envName + ".\" }' --endpoint=" + env.JIRA_SERVER
+}
+
+def getList(project, format) {
+
+    println "Obteniendo lista de elementos del proyecto: " + project
+
+    SH_CMD = sh (
+        script: "jiracli list --template="+ format + " --query \"project = '"+ project +"'\"  --endpoint=" + env.JIRA_SERVER,
+        returnStdout: true
+    )
+    println "${SH_CMD}"
  
+}
+def getIssueByVersion(project, version) {
+ 
+    println "Obteniendo tarjeta de version en JIRA: " + version 
+    SH_CMD = sh (
+ 
+
+        script: '''
+                    set +x  
+                    jiracli list --template table --query "project = \'''' + project + '''\' and affectedVersion IN (\'''' + version + '''\')  ORDER BY priority asc, created" --endpoint=''' + env.JIRA_SERVER + ''' | grep -i APDP | awk -F '|' '{ print $2 }' | sed -e 's/^[[:space:]]*//'
+                    set -x
+                ''',
+        returnStdout: true
+    ).trim()
+
+    if (SH_CMD) { 
+        if (SH_CMD!= "") { 
+          println "La tarjeta asociada es: " + SH_CMD 
+          return SH_CMD
+        }
+    }
+
+       
+      jiraCheckMsg =  "No se encontraron tarjetas activas para el proyecto ${project} y la version ${version}"
+      println jiraCheckMsg
+      //error msg
+      throw new Exception(jiraCheckMsg)
+
+
+     //
+}
